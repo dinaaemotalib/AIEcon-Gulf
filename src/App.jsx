d5da36fbded0d1834cb233243dd1f31279688594
Dina Abd El Motalib <dinaabdelmotalib@gmail.com>
2025-10-31 17:29:37 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+// import React from 'react';
+// import Navbar from '../src/components/Navbar.jsx';
+// import Header from '../src/components/Header.jsx';
+
+// function App() {
+//   return (
+//     <div className="w-full overflow-hidden">
+//       {/* <Navbar /> */}
+//       <Header />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import React from 'react';
-import Navbar from '../src/components/Navbar.jsx';
-import Header from '../src/components/Header.jsx';
 
 function App() {
   return (
-    <div className="w-full overflow-hidden">
-      {/* <Navbar /> */}
-      <Header />
+    <div>
+      <h1>Hello AIEcon Gulf 👋</h1>
+      <p>If you see this, React is working!</p>
     </div>
   );
 }
